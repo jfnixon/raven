f29eb4f221b48e0fd5dae84cb9b70256800246d3
Fred Nixon <fred@johnfnixon.com>
2012-09-03 23:14:26 -0400

    Initial new setup for rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raven::Application.config.secret_token = '91217408ae2e28b0aa12e6337bccd086cc519a9ae8b437a237bf13a50d70babd1d2c2f87e5961e35109bd4623268142ae9b13df24eeee8828a3a6176f2f928b1'

0bbcf437773b8b6c866c7c2e296bd11d2e302009
Fred Nixon <fred@johnfnixon.com>
2012-09-04 22:19:22 -0400

    Add in Devise boilerplate and Twitter Bootstrap
    Vanilla boilerplate from http://railsapps.github.com/twitter-bootstrap-rails.html
    We don't like sass-bootstrap or sass-rails-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Raven::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'static_pages#home'
 end

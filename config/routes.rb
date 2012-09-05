ee2ffe5720c49a9374e9fdf426624170f2f589ed
Fred Nixon <fred@johnfnixon.com>
2012-09-04 22:58:30 -0400

    Add in Static Pages
    Create the StaticPages controller
    Home is the home page
    About
    Help
    Signin/signup/signout via Devise
    Slight rearrangement of application layout
    Inverse bar all across top, fixed
    Footer in light grey, standard width

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Raven::Application.routes.draw do
+  match "home" => "static_pages#home"
+  match "help" => "static_pages#help"
+  match "about" => "static_pages#about"
+
   devise_for :users
 
   # You can have the root of your site routed with "root"
